cb79aba000ff46d1b9cd9306eda348a6d4bf899d
Blackys <ys.heo.korea@gmail.com>
2022-10-04 00:47:35 +0900

    Update helloWorld.test.js

--- 01_backend/test/helloWorld.test.js
+++ 01_backend/test/helloWorld.test.js
@@ -1,35 +1,35 @@
-// const assert = require("chai").assert;
-// const expect = require("chai").expect;
-// const should = require("chai").should();
-// const { helloWolrd } = require("../testApp.js");
+const assert = require("chai").assert;
+const expect = require("chai").expect;
+const should = require("chai").should();
+const { helloWolrd } = require("../testApp.js");
 
-// describe("My First Test", () => {
-//   beforeEach(() => {
-//     console.log("🚀️ test start ...");
-//   });
+describe("My First Test", () => {
+  beforeEach(() => {
+    console.log("🚀️ test start ...");
+  });
 
-//   it("1) Chai Assert Test", () => {
-//     const result = helloWolrd();
-//     assert.equal(result, "hello world");
-//     assert.lengthOf(result, 11);
-//     assert.typeOf(result, 'string')
-//   });
+  it("1) Chai Assert Test", () => {
+    const result = helloWolrd();
+    assert.equal(result, "hello world");
+    assert.lengthOf(result, 11);
+    assert.typeOf(result, 'string')
+  });
 
-//   it("2) Chai Expect Test", () => {
-//     const result = helloWolrd();
-//     expect(result).to.be.a('string');
-//     expect(result).to.equal('hello world');
-//     expect(result).to.have.lengthOf(11);
-//   });
+  it("2) Chai Expect Test", () => {
+    const result = helloWolrd();
+    expect(result).to.be.a('string');
+    expect(result).to.equal('hello world');
+    expect(result).to.have.lengthOf(11);
+  });
 
-//   it("3) Chai Should Test", () => {
-//     const result = helloWolrd();
-//     result.should.be.a('string');
-//     result.should.equal('hello world');
-//     result.should.have.lengthOf(11);
-//   });
+  it("3) Chai Should Test", () => {
+    const result = helloWolrd();
+    result.should.be.a('string');
+    result.should.equal('hello world');
+    result.should.have.lengthOf(11);
+  });
 
-//   afterEach(() => {
-//     console.log("🔨️ test terminated ... ");
-//   });
-// });
+  afterEach(() => {
+    console.log("🔨️ test terminated ... ");
+  });
+});
